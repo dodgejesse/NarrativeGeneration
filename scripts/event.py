25ce0b5ddd82ba50681a3ffb178d0aa6b6596dae
David Bamman <david.bamman@gmail.com>
2015-01-10 18:04:38 -0500

    init

---
+++ scripts/event.py
@@ -0,0 +1,46 @@
+"""
+Class to store data for protest events
+"""
+
+class ProtestEvent:
+
+	# All of these args seem required for any event prediction
+	def __init__(self, eventType, date, location, eventProbs, populationProbs, violenceProbs, confidence):
+		self.date=date
+		self.eventType=eventType
+		self.date=date
+		self.location=location
+		self.confidence=confidence
+
+		##
+		# all probability distributions; keep all in case we want to discuss uncertainty (or if the max doesn't win by much)
+		##
+
+		# all event probabilities
+		self.eventProbs=eventProbs
+		# all population probabilities
+		self.populationProbs=populationProbs
+		# all violence probabilities
+		self.violenceProbs=violenceProbs
+		
+	# pop max value of (k,v) list; or multiple if ties
+	def getMax(self, array):
+		plist=[]
+		(maxPop, maxProb)=array[0]
+		plist.append((maxPop, maxProb))
+		for i in range(1,len(array)):
+			(k,v)=array[i]
+			if v == maxProb:
+				plist.append((k,v))
+
+		return plist
+
+
+	def printData(self):
+		print "Event type: %s" % self.eventType
+		print "Date: %s" % self.date
+		print "Location: %s" % self.location
+		print "Confidence: %s" % self.confidence
+		print "Event: %s" % (self.getMax(self.eventProbs))
+		print "Population: %s" % (self.getMax(self.populationProbs))
+		print "Violence: %s" % (self.getMax(self.violenceProbs))
